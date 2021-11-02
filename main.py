16271809bf3fee278df2abacc71e8d98703a3ddf
sudeepadk <sudeepadr@gmail.com>
2021-11-02 23:42:06 +0000

    Task2

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-# Task1
-import random
-myname = input("Enter your Name: ")
-mynumber = random.randint(1, 10)
 
-print("Hello  " + myname)
-guess = int(input("Enter a number between 1 and 10 : "))
-if guess == mynumber:
-    print("Wow!" +"  " + myname + "You guessed my lucky number")
-else:
-    print("Try again")
+#Task2
+print("Hi")
+your_name = input("Enter your name : ")
+print("Let's start a game  " + your_name )
+import random
+Fav_Num = random.randint(1,100)
+print("choose favourite number between 1 and 100")
+fav = int(input("Enter your number :  "))
+if fav <= 25:
+    joke1 = print("Wow!You are lucky today")
+    print(joke1)
+elif int(fav>25 and fav<=50):
+    joke2 = print("You are just amazing !")
+    print(joke2)
+elif int(fav>50 and fav<=90):
+     joke3 = print("You are just few steps away from a treasure")
+     print(joke3)
+elif int(fav>90 and fav<=100):
+        joke4 = "Congrats !  You are the winner of the day"
+        print(your_name + "   " + joke4)
+elif int(fav>100):
+    joke5 = print("check your number")
+    print(joke5)
